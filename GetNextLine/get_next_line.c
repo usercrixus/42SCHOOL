b9f6058a900b324c5756131c138a03f34c20b1a8
Chaisneau <chaisneau@gmail.com>
2024-11-12 15:48:22 +0100

    code contraction

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,42 +6,16 @@
 /*   By: achaisne <achaisne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 18:58:26 by achaisne          #+#    #+#             */
-/*   Updated: 2024/11/12 14:21:12 by achaisne         ###   ########.fr       */
+/*   Updated: 2024/11/12 15:46:49 by achaisne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-static enum e_sstatus	str_strt(
+static enum e_sstatus	populate_line(
 	char *buffer, t_string *s, ssize_t byte_read, ssize_t *offset)
 {
-	while (buffer[*offset] != '\n' && *offset < byte_read)
-	{
-		if (!push_char(s, buffer[*offset]))
-		{
-			free_string(s);
-			return (FAILED);
-		}
-		(*offset)++;
-	}
-	if (buffer[*offset] == '\n' && *offset < byte_read)
-	{
-		if (!push_char(s, buffer[*offset]))
-		{
-			free_string(s);
-			return (FAILED);
-		}
-		(*offset)++;
-		return (TERMINATED);
-	}
-	return (PENDING);
-}
-
-static enum e_sstatus	str_end(
-	char *buffer, t_string *s, ssize_t byte_read, ssize_t *offset)
-{
-	*offset = 0;
 	while (*offset < byte_read && buffer[*offset] != '\n')
 	{
 		if (!push_char(s, buffer[*offset]))
@@ -64,14 +38,14 @@ static enum e_sstatus	str_end(
 	return (PENDING);
 }
 
-static int	populate_t_string_line(int fd, t_string *str)
+static int	manage_populate_line(int fd, t_string *str)
 {
 	enum e_sstatus		str_status;
 	static char			buffer[BUFFER_SIZE];
 	static ssize_t		byte_read;
 	static ssize_t		offset;
 
-	str_status = str_strt(buffer, str, byte_read, &offset);
+	str_status = populate_line(buffer, str, byte_read, &offset);
 	if (str_status == FAILED)
 		return (0);
 	else if (str_status == PENDING)
@@ -79,7 +53,8 @@ static int	populate_t_string_line(int fd, t_string *str)
 		byte_read = read(fd, buffer, BUFFER_SIZE);
 		while (byte_read > 0)
 		{
-			str_status = str_end(buffer, str, byte_read, &offset);
+			offset = 0;
+			str_status = populate_line(buffer, str, byte_read, &offset);
 			if (str_status == FAILED)
 				return (0);
 			else if (str_status == TERMINATED)
@@ -98,7 +73,7 @@ char	*get_next_line(int fd)
 	str = create_string();
 	if (!str)
 		return (0);
-	if (!populate_t_string_line(fd, str))
+	if (!manage_populate_line(fd, str))
 	{
 		free(str);
 		return (0);
